0a7bf7682b12a6380b522a0da773eedf5ac27899
Jay Tailor <tailorj64@gmail.com>
2023-08-07 18:37:54 +0530

    Docker setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { LessonModule } from './lesson/lesson.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://root:password@mongo:27017/school?directConnection=true&authSource=admin&replicaSet=replicaset&retryWrites=true',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [
+        Lesson,
+      ],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: true,
