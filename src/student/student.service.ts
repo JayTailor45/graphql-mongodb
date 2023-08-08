7cc8e9a92cda57de5c782cca75ce27b31832d71c
Jay Tailor <tailorj64@gmail.com>
2023-08-08 14:02:44 +0530

    Get student by id

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -7,22 +7,27 @@ import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class StudentService {
+  constructor(
+    @InjectRepository(Student) private studentRepo: Repository<Student>,
+  ) {}
 
-    constructor(
-        @InjectRepository(Student) private studentRepo: Repository<Student>,
-    ) {}
+  async getStudents(): Promise<Student[]> {
+    return this.studentRepo.find();
+  }
 
-    async getStudents(): Promise<Student[]> {
-        return this.studentRepo.find();
-    }
+  async getStudent(id: string): Promise<Student> {
+    return this.studentRepo.findOne({ where: { id } });
+  }
 
-    async createStudent(createStudentInput: CreateStudentInput): Promise<Student> {
-        const {firstName, lastName} = createStudentInput;
-        const student = this.studentRepo.create({
-            id: uuid(),
-            firstName,
-            lastName,
-        });
-        return this.studentRepo.save(student);
-    }
+  async createStudent(
+    createStudentInput: CreateStudentInput,
+  ): Promise<Student> {
+    const { firstName, lastName } = createStudentInput;
+    const student = this.studentRepo.create({
+      id: uuid(),
+      firstName,
+      lastName,
+    });
+    return this.studentRepo.save(student);
+  }
 }
